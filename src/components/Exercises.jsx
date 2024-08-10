18ad49169f2c6131aca94d1e9ed9760d4de2bffd
Vaibhav Samdani <vaibhavmaheshwari517@gmail.com>
2024-08-10 23:09:38 +0530

    minor updates

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import gym from "../assets/gym.svg";
-import ExercisesResult from "./ExercisesResult";
 const Exercises = () => {
   return (
     <>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Home from "../components/Home";
-import Exercises from "../components/Exercises";
 import ExercisesResult from "../components/ExercisesResult";
 
 const LandingPage = () => {
